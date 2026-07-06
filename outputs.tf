fe21bee4f1fe0ff162374a814efae1b23d6895df
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "database_migration_services" {
   description = "All database_migration_service resources"
   value       = azurerm_database_migration_service.database_migration_services
 }
+output "database_migration_services_location" {
+  description = "List of location values across all database_migration_services"
+  value       = [for k, v in azurerm_database_migration_service.database_migration_services : v.location]
+}
+output "database_migration_services_name" {
+  description = "List of name values across all database_migration_services"
+  value       = [for k, v in azurerm_database_migration_service.database_migration_services : v.name]
+}
+output "database_migration_services_resource_group_name" {
+  description = "List of resource_group_name values across all database_migration_services"
+  value       = [for k, v in azurerm_database_migration_service.database_migration_services : v.resource_group_name]
+}
+output "database_migration_services_sku_name" {
+  description = "List of sku_name values across all database_migration_services"
+  value       = [for k, v in azurerm_database_migration_service.database_migration_services : v.sku_name]
+}
+output "database_migration_services_subnet_id" {
+  description = "List of subnet_id values across all database_migration_services"
+  value       = [for k, v in azurerm_database_migration_service.database_migration_services : v.subnet_id]
+}
+output "database_migration_services_tags" {
+  description = "List of tags values across all database_migration_services"
+  value       = [for k, v in azurerm_database_migration_service.database_migration_services : v.tags]
+}
 
